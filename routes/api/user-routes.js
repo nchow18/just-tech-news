b609e089ed361fe132014e362de8e8392a70e49f
Nathan Chow <emailme@nathanchow.ca>
2021-03-13 14:13:28 -0500

    update files

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -14,32 +14,25 @@ router.get('/', (req, res) => {
       });
   });
 
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password']},
-    where: { id: req.params.id }
-  })
-  .then(dbUserdata => {
-
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' })
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  })
-})
-
-
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
